715064806ec5687475bc5a12910748953d73fff7
erob <eric.robinson@afwerx.af.mil>
2021-05-02 20:17:53 -0700

    mobile avoid resize form weird hubspot behavior

--- js/forms.js
+++ js/forms.js
@@ -1,2 +1,9 @@
+//gets screen size, we don't resize for mobile
+const width  = window.innerWidth || document.documentElement.clientWidth || 
+document.body.clientWidth;
+
+// const height = window.innerHeight|| document.documentElement.clientHeight|| 
+// document.body.clientHeight;
+
 // handles the weird iframe hard-style width...seriously hubspot?
-window.onresize = function(){ location.reload(); }
+(width > 600) ? window.onresize = function(){ location.reload(); } : "";
